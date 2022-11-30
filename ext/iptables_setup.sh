fad04c8165c5a8e0f85a32a3e7f584284bdfbfc9
rodfer0x80 <rodfer0x80@proton.me>
2022-11-30 09:59:02 +0000

    .

--- ext/iptables_setup.sh
+++ ext/iptables_setup.sh
@@ -2,12 +2,12 @@
 
 #
 # Notes: if you are behind a NAT, you will also need to do the following:
-# IFACE=$(echo $(ip r | grep default) | cut -d " " -f5)
-# iptables -t nat -A POSTROUTING  -s 10.8.0.0/24 -o $IFACE -j MASQUERADE
+IFACE=$(echo $(ip r | grep default) | cut -d " " -f5)
+iptables -t nat -A POSTROUTING  -s 10.8.0.0/24 -o $IFACE -j MASQUERADE
 #
 
 sudo iptables -I INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 sudo iptables -I INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-sudo iptables -I INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+sudo iptables -I INPUT -p tcp -m tcp --dport 1194 -j ACCEPT
 sudo iptables -I INPUT -p tcp -m tcp --dport 53 -j ACCEPT
 sudo iptables -I INPUT -p tcp -m tcp --dport all -j DENY
